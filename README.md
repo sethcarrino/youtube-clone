42de6b0ca528b1d60eca519b72af91f2878c93e4
sethcarrino <sethcarrino@gmail.com>
2017-08-15 20:36:58 -0400

    Initial Commit - setup some components - started pulled data using youtube's API

--- README.md
+++ README.md
@@ -1,25 +1,17 @@
-# ReduxSimpleStarter
+# YouTube Clone
 
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
+First project using React. Created a YouTube Clone with YouTube's API and React.
 
 ### Getting Started
 
 There are two methods for getting started with this repo.
 
-#### Familiar with Git?
+#### Download and try it out
 Checkout this repo, install dependencies, then start the gulp process with the following:
 
 ```
-> git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
-> cd ReduxSimpleStarter
-> npm install
-> npm start
-```
-
-#### Not Familiar with Git?
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
-```
+> git clone
+> cd youtube-clone
 > npm install
 > npm start
 ```
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBLax3bEf5YLPL203jezcISbaXVjzwdZ4I';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create new component. This component
+// should produce some HTML.
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // same as this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div >
+        <SearchBar / >
+        <VideoList videos={this.state.videos} / >
+      </div>
+    );
+  }
+}
+
+// Take this component 's generated HTML and put it on the page or in DOM
+ReactDOM.render( < App / > , document.querySelector('.container'));
